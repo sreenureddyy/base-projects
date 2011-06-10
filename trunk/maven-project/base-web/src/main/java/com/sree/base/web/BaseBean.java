11440ff9f68f4e31db10a01aabca24bb334ba103
sreenureddy.y@gmail.com <sreenureddy.y@gmail.com@502667d9-03ed-43e4-a4ba-09df657ea129>
2011-06-10 12:39:49 +0000

    Setup the base project in svn

---
+++ trunk/maven-project/base-web/src/main/java/com/sree/base/web/BaseBean.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.sree.base.web;
+
+/**
+ * @author srinivasr
+ *
+ */
+public class BaseBean {
+	@SuppressWarnings("unused")
+	private String string = new String();
+
+	public String getString() {
+		return "Hello Sreeeee";
+	}
+
+	public void setString(String string) {
+		this.string = string;
+	}
+	
+	
+}
