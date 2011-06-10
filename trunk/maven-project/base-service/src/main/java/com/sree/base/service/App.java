64fb1640d8dd3fe433ee3ea70808390ccccc7875
sreenureddy.y <sreenureddy.y@gmail.com>
2011-06-10 12:39:49 +0000

    Setup the base project in svn

---
+++ trunk/maven-project/base-service/src/main/java/com/sree/base/service/App.java
@@ -0,0 +1,13 @@
+package com.sree.base.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
