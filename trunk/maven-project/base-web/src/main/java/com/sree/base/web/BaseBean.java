564380336a0810e626e0722528ad4173698b40cf
sreenureddy.y <sreenureddy.y@gmail.com>
2011-06-17 13:07:50 +0000

    Added Richfaces, Hibernate, spring, ehcache capabulities

--- trunk/maven-project/base-web/src/main/java/com/sree/base/web/BaseBean.java
+++ trunk/maven-project/base-web/src/main/java/com/sree/base/web/BaseBean.java
@@ -3,16 +3,21 @@
  */
 package com.sree.base.web;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
 /**
  * @author srinivasr
  *
  */
+@Component("baseBean")
+@Scope(value = "session")
 public class BaseBean {
 	@SuppressWarnings("unused")
 	private String string = new String();
 
 	public String getString() {
-		return "Hello Sreeeee";
+		return "Hello Sreeeee....";
 	}
 
 	public void setString(String string) {
