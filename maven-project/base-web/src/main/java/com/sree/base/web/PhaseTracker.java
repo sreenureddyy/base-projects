6dab79430b6e6c4bc2e7950eddc16a0854f591f9
sreenureddy.y <sreenureddy.y@gmail.com>
2011-07-03 07:15:48 +0000

    adding richfaces

---
+++ maven-project/base-web/src/main/java/com/sree/base/web/PhaseTracker.java
@@ -0,0 +1,39 @@
+package com.sree.base.web;
+
+import java.text.MessageFormat;
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+import org.richfaces.log.LogFactory;
+import org.richfaces.log.Logger;
+
+public class PhaseTracker implements PhaseListener {
+	private static final long serialVersionUID = 6358081870120864332L;
+	private Logger logger = LogFactory.getLogger(PhaseTracker.class);
+	private ThreadLocal<Long> phaseTimer = new ThreadLocal();
+
+	public void afterPhase(PhaseEvent event) {
+		Long phaseStartTime = (Long) this.phaseTimer.get();
+
+		long measuredTime = 0L;
+		if (phaseStartTime != null) {
+			measuredTime = System.currentTimeMillis()
+					- phaseStartTime.longValue();
+		}
+
+		this.logger.debug(MessageFormat
+				.format("Phase {0} completed by {1}ms", new Object[] {
+						event.getPhaseId(), Long.valueOf(measuredTime) }));
+		this.phaseTimer.set(null);
+	}
+
+	public void beforePhase(PhaseEvent event) {
+		this.logger.debug(MessageFormat.format("Phase {0} started",
+				new Object[] { event.getPhaseId() }));
+		this.phaseTimer.set(Long.valueOf(System.currentTimeMillis()));
+	}
+
+	public PhaseId getPhaseId() {
+		return PhaseId.ANY_PHASE;
+	}
+}
