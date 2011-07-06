2dd6d5a1c659208e45135ece5d474a814c1f8231
sreenureddyy@gmail.com <sreenureddyy@gmail.com@502667d9-03ed-43e4-a4ba-09df657ea129>
2011-07-06 13:33:36 +0000

    Log4j integration with junit and application

--- trunk/maven-project/project/src/test/java/com/sree/test/TestUser.java
+++ trunk/maven-project/project/src/test/java/com/sree/test/TestUser.java
@@ -5,6 +5,7 @@ package com.sree.test;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 /**
@@ -13,10 +14,12 @@ import org.junit.Test;
  */
 @SuppressWarnings("unchecked")
 public class TestUser extends BaseTest {
+	private Logger log = Logger.getLogger(TestUser.class);
 	@Test
 	public void testCase() {
 		List<String> usernames = baseService.find("getusers");
-		System.out.println(usernames.size());
-		System.out.println(usernames.get(0));
-	}
+		log.info(usernames.size());
+		log.info(usernames.get(0));
+	} 
 }
+    
