9536168588f3997cd278538f49999bd2c990f421
sreenureddyy@gmail.com <sreenureddyy@gmail.com@502667d9-03ed-43e4-a4ba-09df657ea129>
2011-07-06 09:40:46 +0000

    Integrating Junit

---
+++ trunk/maven-project/project/src/test/java/com/sree/test/BaseTest.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.sree.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.sree.base.service.IBaseService;
+
+/**
+ * @author srinivasr
+ * 
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+		"classpath:/META-INF/spring/springContext-Test.xml",
+		"classpath:/META-INF/spring/applicationContext-security.xml",
+		"classpath:/META-INF/spring/applicationContext-spring.xml" })
+public class BaseTest {
+	@Autowired
+	IBaseService baseService;
+}
