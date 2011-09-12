35f4ed48e47f86cd6d3f6fe30b3def72caad7e6d
sreenureddy.y <sreenureddy.y@gmail.com>
2011-09-12 03:59:27 +0000

    user domains created

--- trunk/maven-project/base-domain/src/main/java/com/sree/base/domain/LookupValue.java
+++ trunk/maven-project/base-domain/src/main/java/com/sree/base/domain/LookupValue.java
@@ -3,25 +3,48 @@
  */
 package com.sree.base.domain;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 /**
  * @author Sree
  *
  */
 @SuppressWarnings("serial")
+@Entity
+@Table(name="LOOKUP_VALUE")
 public class LookupValue extends BaseDomain {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ID")
 	private Long id;
 
+	@Column(name="LOOKUP_CODE")
 	private String lookupCode;
+	
+	@Column(name="LOOKUP_VALUE")
 	private String lookupValue;
+	
+	@Column(name="DESCRIPTION")
 	private String description;
 	
+	@Column(name="SORT_ORDER")
 	private int sortOrder;
-	
+
+	@JoinColumn(name = "LOOKUP_CATEGORY")
+	@ManyToOne(cascade = CascadeType.ALL)
 	private LookupCategory lookupCategory = new LookupCategory();
 	
-	private Boolean isActive;
-	private Boolean isModifiable;
+	@Column(name="ISACTIVE")
+	private Boolean isActive = true;
 	
 	public Long getId() {
 		return id;
@@ -65,10 +88,5 @@ public class LookupValue extends BaseDomain {
 	public void setIsActive(Boolean isActive) {
 		this.isActive = isActive;
 	}
-	public Boolean getIsModifiable() {
-		return isModifiable;
-	}
-	public void setIsModifiable(Boolean isModifiable) {
-		this.isModifiable = isModifiable;
-	}
+	
 }
