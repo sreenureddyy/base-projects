1e7b8ca44342ca8c701e8a3c3791ec09c535f6f4
sreenureddy.y <sreenureddy.y@gmail.com>
2012-07-15 16:19:08 +0000

    Country bean

--- branches/pharmaci/base-service/src/main/java/com/sree/service/user/UserService.java
+++ branches/pharmaci/base-service/src/main/java/com/sree/service/user/UserService.java
@@ -9,6 +9,7 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
+import com.sree.base.domain.User;
 import com.sree.base.service.BaseService;
 import com.sree.base.service.IBaseService;
 
@@ -29,6 +30,16 @@ public class UserService extends BaseService implements IUserService {
 	@Override
 	public List<?> getUser(String queryName, Object... objects) {
 		return baseService.find(queryName, objects);
+	}
+
+	@Override
+	public void saveUser(User user) {
+		baseService.save(user);
+	}
+
+	@Override
+	public void deleteUser(User user) {
+		baseService.delete(user);		
 	}
 
 }
