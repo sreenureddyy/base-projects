4e9aec0d944e561ce657e70adc96d49d0056403c
sreenureddy.y <sreenureddy.y@gmail.com>
2012-10-15 11:30:53 +0000

    Resolved Test issues

--- branches/BaseProject/project/src/test/java/com/sree/test/BaseTest.java
+++ branches/BaseProject/project/src/test/java/com/sree/test/BaseTest.java
@@ -3,12 +3,15 @@
  */
 package com.sree.test;
 
+import org.apache.log4j.Logger;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.sree.base.service.IBaseService;
+import com.sree.test.TestUser;
 
 /**
  * @author srinivasr
@@ -20,6 +23,13 @@ import com.sree.base.service.IBaseService;
 		"/META-INF/spring/applicationContext-security.xml",
 		"/META-INF/spring/applicationContext-spring.xml" })
 public class BaseTest {
+	private Logger log = Logger.getLogger(BaseTest.class);
+	
 	@Autowired
 	IBaseService baseService;
+	
+	@Test
+	public void testInit() {
+		log.info("Initilization of test suit");
+	}
 }
